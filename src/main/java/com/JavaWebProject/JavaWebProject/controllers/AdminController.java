0ab33b5bfa2a1d684cc9bc3c03feee5082a1baf0
dradvm <duynnvn2511@gmail.com>
2024-05-15 20:11:07 +0700

    add UI admin page

---
+++ src/main/java/com/JavaWebProject/JavaWebProject/controllers/AdminController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.JavaWebProject.JavaWebProject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author DELL
+ */
+@Controller
+@RequestMapping(value = "/admin")
+public class AdminController {  
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String adminPage() {
+        return "AdminPage/admin";
+    }
+}
