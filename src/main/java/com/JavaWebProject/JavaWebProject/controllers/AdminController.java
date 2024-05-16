458598eb8b1ed077cbba7e4163736164df08a991
dradvm <duynnvn2511@gmail.com>
2024-05-17 00:32:14 +0700

    add thymeleaf layout dialect + admin layout UI

--- src/main/java/com/JavaWebProject/JavaWebProject/controllers/AdminController.java
+++ src/main/java/com/JavaWebProject/JavaWebProject/controllers/AdminController.java
@@ -4,12 +4,15 @@
  */
 package com.JavaWebProject.JavaWebProject.controllers;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,28 +26,51 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "/admin")
 public class AdminController {  
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public String adminPage() {
-        
-        return "AdminPage/admin";
+    private ArrayList<LocalDate> days;
+    private ArrayList<Month> months;
+    private ArrayList<Integer> years;
+    private String[] labelsDay;
+    private String[] labelsMonth;
+    private String[] labelsYear;
+    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+    public String adminPage(Model model) {
+        days = new ArrayList<>();
+        months = new ArrayList<>();
+        years = new ArrayList<>();
+        for (int i = 6; i >=0; i--) {
+            days.add(LocalDate.now().minusDays(i));
+            months.add(LocalDate.now().getMonth().minus(i));
+            years.add(LocalDate.now().getYear() - i);
+        }
+        labelsDay = days.stream().map(day -> 
+            String.valueOf(day.getDayOfMonth())+
+            "/"+
+            String.valueOf(day.getMonthValue())
+        ).toArray(String[]::new);
+        labelsMonth = months.stream().map(month ->
+            String.valueOf(month.getValue())
+        ).toArray(String[]::new);
+        labelsYear = years.stream().map(year ->
+            String.valueOf(year)
+        ).toArray(String[]::new);
+        return "AdminPage/admindashboard";
     }
     @GetMapping("/lineChart")
     @ResponseBody
     public Map<String, Object> getDataLineChart(@RequestParam("selectedValue") String selectedValue) {
         // Xử lý logic dựa trên selectedValue
         Map<String, Object> data = new HashMap<>();
-
         switch (selectedValue) {
             case "Day":
-                data.put("labels", new String[]{"January", "February", "March"});
-                data.put("data", new int[]{10, 50, 30});
+                data.put("labels", labelsDay);
+                data.put("data", new int[]{100, 50, 100});
                 break;
             case "Month":
-                data.put("labels", new String[]{"April", "May", "June"});
+                data.put("labels", labelsMonth);
                 data.put("data", new int[]{40, 200, 60});
                 break;
             case "Year":
-                data.put("labels", new String[]{"July", "August", "September"});
+                data.put("labels", labelsYear);
                 data.put("data", new int[]{70, 500, 90});
                 break;
             default:
@@ -75,31 +101,29 @@ public class AdminController {
         datasets.add(commission);
         switch (selectedValue) {
             case "Day":
-                data.put("labels", new String[]{"Red", "Blue", "Yellow", "Green", "Purple", "Orange"});
+                data.put("labels", labelsDay);
                 advertise.put("data", new int[]{1, 2, 3, 4, 5, 6});
                 rank.put("data", new int[]{7, 8, 9, 10, 11, 12});
                 commission.put("data", new int[]{1,1,1,1,1,1});
-                data.put("datasets", datasets);
                 break;
             case "Month":
-                data.put("labels", new String[]{"Red", "Blue", "Yellow", "Green", "Purple", "Orange"});
+                data.put("labels", labelsMonth);
                 advertise.put("data", new int[]{1, 2, 3, 4, 5, 6});
                 rank.put("data", new int[]{7, 8, 9, 10, 11, 12});
                 commission.put("data", new int[]{1,1,1,1,1,1});
-                data.put("datasets", datasets);
                 break;
             case "Year":
-                data.put("labels", new String[]{"Red", "Blue", "Yellow", "Green", "Purple", "Orange"});
+                data.put("labels", labelsYear);
                 advertise.put("data", new int[]{1, 2, 3, 4, 5, 6});
                 rank.put("data", new int[]{7, 8, 9, 10, 11, 12});
                 commission.put("data", new int[]{1,1,1,1,1,1});
-                data.put("datasets", datasets);
                 break;
             default:
                 data.put("labels", new String[]{});
                 data.put("datasets", new int[]{});
                 break;
         }
+        data.put("datasets", datasets);
         return data;
     }
 }
