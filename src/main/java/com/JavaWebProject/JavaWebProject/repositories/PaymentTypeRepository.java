a37cc7fa1d173ac023a81bd93595de5c96c3b8dc
dradvm <duynnvn2511@gmail.com>
2024-05-20 21:33:34 +0700

    fix conflict

--- src/main/java/com/JavaWebProject/JavaWebProject/repositories/PaymentTypeRepository.java
+++ src/main/java/com/JavaWebProject/JavaWebProject/repositories/PaymentTypeRepository.java
@@ -8,15 +8,6 @@ import com.JavaWebProject.JavaWebProject.models.PaymentType;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- *
- * @author DELL
- */
-<<<<<<< HEAD:src/main/java/com/JavaWebProject/JavaWebProject/repositories/PaymentTypeRepository.java
 public interface PaymentTypeRepository extends CrudRepository<PaymentType, Long> {
     List<PaymentType> findAll();
-=======
-public interface OrderRepository extends CrudRepository<CateringOrder, Integer> {
-   
->>>>>>> 1036a975d3fdde849f70e6b38410336413320847:src/main/java/com/JavaWebProject/JavaWebProject/repositories/OrderRepository.java
 }
