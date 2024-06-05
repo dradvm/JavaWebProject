bbf8cef3685398e3e4f2cdf475f99650bf14dabf
Loc09 <142898685+Loc09@users.noreply.github.com>
2024-06-05 16:47:27 +0700

    fix

--- src/main/java/com/JavaWebProject/JavaWebProject/controllers/CatererController.java
+++ src/main/java/com/JavaWebProject/JavaWebProject/controllers/CatererController.java
@@ -4,34 +4,17 @@
  */
 package com.JavaWebProject.JavaWebProject.controllers;
 
-<<<<<<< HEAD
-import com.JavaWebProject.JavaWebProject.models.Banner;
-import com.JavaWebProject.JavaWebProject.models.BannerType;
-import com.JavaWebProject.JavaWebProject.models.Caterer;
-import com.JavaWebProject.JavaWebProject.models.Dish;
-import com.JavaWebProject.JavaWebProject.services.BannerManageService;
-import com.JavaWebProject.JavaWebProject.services.BannerTypeService;
-=======
 import com.JavaWebProject.JavaWebProject.models.Caterer;
 import com.JavaWebProject.JavaWebProject.models.Dish;
 import com.JavaWebProject.JavaWebProject.models.District;
->>>>>>> bac2ad2e3d342cc787d787dc64e02818598ede13
 import com.JavaWebProject.JavaWebProject.services.CatererService;
 import com.JavaWebProject.JavaWebProject.services.CloudStorageService;
 import com.JavaWebProject.JavaWebProject.services.DishService;
 import com.JavaWebProject.JavaWebProject.services.DistrictService;
 import com.JavaWebProject.JavaWebProject.services.OrderDetailsService;
-<<<<<<< HEAD
-import jakarta.servlet.http.HttpSession;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Calendar;
-=======
 import java.math.BigDecimal;
 import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
->>>>>>> bac2ad2e3d342cc787d787dc64e02818598ede13
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -57,7 +40,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 @RequestMapping(value = "/caterer")
 public class CatererController {
-
     @Autowired
     private AuthController user;
     @Autowired
@@ -69,30 +51,26 @@ public class CatererController {
     @Autowired
     private CloudStorageService cloudStorageService;
     @Autowired
-<<<<<<< HEAD
-    private BannerManageService bannerManageService;
-    @Autowired
-    private BannerTypeService bannerTypeService;
-
-=======
     private HttpSession session;
     @Autowired
     private DistrictService districtService;
+    @Autowired
+    private BannerManageService bannerManageService;
+    @Autowired
+    private BannerTypeService bannerTypeService;
     
->>>>>>> bac2ad2e3d342cc787d787dc64e02818598ede13
     @GetMapping(value = "/myCaterer/orders")
     public String orderPage(ModelMap model) {
         model.addAttribute("selectedNav", "myCaterer");
         model.addAttribute("selectedPage", "catererorders");
         return "CatererPage/OrderPage/catererorders";
     }
-
     @GetMapping(value = "/myCaterer/dishes")
     public String dishPage(ModelMap model) {
-
+        
         ArrayList data = new ArrayList<>();
         Map<String, Object> temp;
-        if (user != null) {
+        if (user != null ) {
             for (Dish d : dishService.findAllDishOfCaterer(catererService.findById(user.getUsername()), 1)) {
                 temp = new HashMap();
                 temp.put("dish", d);
@@ -111,30 +89,24 @@ public class CatererController {
         model.addAttribute("selectedPage", "catererdishes");
         return "CatererPage/DishesPage/catererdishes";
     }
-
     @GetMapping("/editDish")
-    public String editDishPage(@RequestParam("ID") int dishID, ModelMap model) {
+    public String editDishPage(@RequestParam("ID") int dishID,ModelMap model) {
         Dish dish = dishService.findByDishID(dishID);
         dish.setDishImage(cloudStorageService.getDishImg(dish.getDishImage()));
         model.addAttribute("dish", dish);
-
+        
         return addAndUpdateDishPage(model);
     }
-
+    
     @GetMapping("/myCaterer/dishes/addAndUpdateDishPage")
     public String addAndUpdateDishPage(ModelMap model) {
         model.addAttribute("selectedNav", "myCaterer");
         model.addAttribute("selectedPage", "catererdishes");
         return "CatererPage/DishesPage/catererdishesaddupdate";
     }
-<<<<<<< HEAD
-
-    public void addAndUpdateDish(Dish dish, String dishName, MultipartFile dishImage, String dishDescription, float dishPrice) {
-=======
         
     
     public void addAndUpdateDish(Dish dish, String dishName, MultipartFile dishImage, String dishDescription, double dishPrice ) {
->>>>>>> bac2ad2e3d342cc787d787dc64e02818598ede13
         dish.setCatererEmail(catererService.findById(user.getUsername()));
         dish.setDishDescription(dishDescription);
         dish.setDishName(dishName);
@@ -144,15 +116,10 @@ public class CatererController {
             String fileName = cloudStorageService.generateFileName(dishImage, user.getUsername());
             dish.setDishImage(fileName);
             cloudStorageService.uploadFile("dish/" + fileName, dishImage);
-<<<<<<< HEAD
-        }
-
-=======
         } 
->>>>>>> bac2ad2e3d342cc787d787dc64e02818598ede13
         dishService.save(dish);
     }
-
+    
     @PostMapping("/addDish")
     public String addDish(
             @RequestParam("dishName") String dishName,
@@ -163,16 +130,17 @@ public class CatererController {
             RedirectAttributes redirectAttributes
     ) {
         Dish dish = new Dish();
-
+        
         if (dishService.isReachMaxDish(catererService.findById(user.getUsername()))) {
             redirectAttributes.addFlashAttribute("message", "You have reached max dish!!");
-        } else {
+        }
+        else {
             addAndUpdateDish(dish, dishName, dishImage, dishDescription, dishPrice);
         }
-
+        
+        
         return "redirect:/caterer/myCaterer/dishes";
     }
-
     @PostMapping("/updateDish")
     public String updateDish(
             @RequestParam("dishID") int dishID,
@@ -184,24 +152,153 @@ public class CatererController {
     ) {
         Dish dish = dishService.findByDishID(dishID);
         addAndUpdateDish(dish, dishName, dishImage, dishDescription, dishPrice);
-
+        
         return "redirect:/caterer/myCaterer/dishes";
     }
-
+    
+    
     @GetMapping(value = "/deleteDish")
     public String deleteDish(@RequestParam int ID) {
         Dish dish = dishService.findByDishID(ID);
-        if (orderDetailsService.isExistDishInOrderDetails(ID)) {
+        if (orderDetailsService.isExistDishInOrderDetails(ID)) { 
             dish.setDishStatus(0);
             dishService.save(dish);
-        } else {
+        }
+        else {
             cloudStorageService.deleteFile("dish/" + dish.getDishImage());
             dishService.delete(dish);
         }
         return "redirect:/caterer/myCaterer/dishes";
     }
-<<<<<<< HEAD
-
+    
+    @GetMapping(value = "/myCaterer/dishes/barChartDish")
+    @ResponseBody
+    public ArrayList getDataLineChartDish() {
+        ArrayList data = new ArrayList<>();
+        Map<String, Object> temp = new HashMap<>();
+        for (Dish d : dishService.findAllDishOfCaterer(catererService.findById(user.getUsername()), 1)) {
+            temp = new HashMap<>();
+            temp.put("name", d.getDishName());
+            temp.put("quantity", orderDetailsService.countQuanitySuccessWithDish(d.getDishID()));
+            temp.put("order", orderDetailsService.countOrderSuccessWithDish(d.getDishID()));
+            
+            data.add(temp);
+        }
+        return data;
+    }
+    
+    @GetMapping(value = "/myCaterer/dishes/report")
+    public String reportPage(ModelMap model) {
+        
+        model.addAttribute("selectedNav", "myCaterer");
+        model.addAttribute("selectedPage", "catererdishes");
+        return "/CatererPage/DishesPage/catererdishesreport";
+    }
+    
+    @PostMapping(value = "/editProfile")
+    @ResponseBody
+    public Map<String, Object> editProfile(
+            @RequestParam("name") String name,
+            @RequestParam("profileImg") MultipartFile profileImg,
+            @RequestParam("paymentInformation") String paymentInformation,
+            @RequestParam("description") String description,
+            @RequestParam("phone") String phone,
+            @RequestParam("gender") int gender,
+            @RequestParam("address") String address,
+            @RequestParam("districtID") int districtID,
+            @RequestParam("birthday") String birthday) {
+        Map<String, Object> result = new HashMap();
+        AuthController auth = (AuthController) session.getAttribute("scopedTarget.authController");
+        Caterer caterer = catererService.findById(auth.getUsername());
+        if (!profileImg.isEmpty()) {
+            String type = profileImg.getContentType();
+            if (type == null || type.equals("application/octet-stream")) {
+                result.put("status", "Invalid");
+                return result;
+            } else if (!type.equals("image/jpeg") && !type.equals("image/png")) {
+                result.put("status", "Invalid");
+                return result;
+            }
+            if (profileImg.getSize() > 10000000) {
+                result.put("status", "Invalid");
+                return result;
+            }
+        }
+        if (name == null || name.trim().length() == 0) {
+            result.put("status", "Invalid");
+            return result;
+        }
+        Pattern pattern = Pattern.compile("^[A-Za-z0-9]+ ([0-9]+ ?){1,}$");
+        if (paymentInformation == null || !pattern.matcher(paymentInformation).matches()) {
+            result.put("status", "Invalid");
+            return result;
+        }
+        pattern = Pattern.compile("^(?:[0-9] ?){7,11}$");
+        if (phone == null || !pattern.matcher(phone).matches()) {
+            result.put("status", "Invalid");
+            return result;
+        }
+        if (gender != 0 && gender != 1) {
+            result.put("status", "Invalid");
+            return result;
+        }
+        if (address == null || address.trim().length() == 0) {
+            result.put("status", "Invalid");
+            return result;
+        }
+        District district = districtService.findById(districtID);
+        if (district == null) {
+            result.put("status", "Invalid");
+            return result;
+        }
+        Date parsedBirthday = new Date();
+        if (birthday != null && birthday.trim().length() > 0) {
+            String[] arr = birthday.split("-");
+            try {
+                int year = Integer.parseInt(arr[0]);
+                int month = Integer.parseInt(arr[1]);
+                int day = Integer.parseInt(arr[2]);
+                parsedBirthday = new Date(year - 1900, month - 1, day);
+            } catch (Exception e) {
+                e.printStackTrace();
+                result.put("status", "Invalid");
+                return result;
+            }
+        } 
+        else {
+            parsedBirthday = null;
+        }
+        if (!profileImg.isEmpty()) {
+            if (caterer.getProfileImage() != null) {
+                if (!cloudStorageService.deleteFile("caterer/" + caterer.getProfileImage())) {
+                    result.put("status", "Fail");
+                    return result;
+                }
+            }
+            String fileName = cloudStorageService.generateFileName(profileImg);
+            if (cloudStorageService.uploadFile("caterer/" + fileName, profileImg)) {
+                caterer.setProfileImage(fileName);
+            } 
+            else {
+                result.put("status", "Fail");
+                return result;
+            }
+        }
+        caterer.setFullName(name);
+        caterer.setPaymentInformation(paymentInformation);
+        caterer.setDescription(description);
+        caterer.setPhone(phone);
+        caterer.setGender(gender);
+        caterer.setAddress(address);
+        caterer.setDistrictID(district);
+        if (parsedBirthday != null) {
+            caterer.setBirthday(parsedBirthday);
+        }
+        catererService.save(caterer);
+        result.put("status", "OK");
+        result.put("target", "/auth/toProfile");
+        return result;
+    }
     @GetMapping("/myCaterer/banners")
     public String bannerPage(ModelMap model, HttpSession session) {
 
@@ -340,136 +437,4 @@ public class CatererController {
         bannerManageService.delete(banner);
         return "redirect:/caterer/myCaterer/banners";
     }
-
-=======
-    
-    @GetMapping(value = "/myCaterer/dishes/barChartDish")
-    @ResponseBody
-    public ArrayList getDataLineChartDish() {
-        ArrayList data = new ArrayList<>();
-        Map<String, Object> temp = new HashMap<>();
-        for (Dish d : dishService.findAllDishOfCaterer(catererService.findById(user.getUsername()), 1)) {
-            temp = new HashMap<>();
-            temp.put("name", d.getDishName());
-            temp.put("quantity", orderDetailsService.countQuanitySuccessWithDish(d.getDishID()));
-            temp.put("order", orderDetailsService.countOrderSuccessWithDish(d.getDishID()));
-            
-            data.add(temp);
-        }
-        return data;
-    }
-    
-    @GetMapping(value = "/myCaterer/dishes/report")
-    public String reportPage(ModelMap model) {
-        
-        model.addAttribute("selectedNav", "myCaterer");
-        model.addAttribute("selectedPage", "catererdishes");
-        return "/CatererPage/DishesPage/catererdishesreport";
-    }
-    
-    @PostMapping(value = "/editProfile")
-    @ResponseBody
-    public Map<String, Object> editProfile(
-            @RequestParam("name") String name,
-            @RequestParam("profileImg") MultipartFile profileImg,
-            @RequestParam("paymentInformation") String paymentInformation,
-            @RequestParam("description") String description,
-            @RequestParam("phone") String phone,
-            @RequestParam("gender") int gender,
-            @RequestParam("address") String address,
-            @RequestParam("districtID") int districtID,
-            @RequestParam("birthday") String birthday) {
-        Map<String, Object> result = new HashMap();
-        AuthController auth = (AuthController) session.getAttribute("scopedTarget.authController");
-        Caterer caterer = catererService.findById(auth.getUsername());
-        if (!profileImg.isEmpty()) {
-            String type = profileImg.getContentType();
-            if (type == null || type.equals("application/octet-stream")) {
-                result.put("status", "Invalid");
-                return result;
-            } else if (!type.equals("image/jpeg") && !type.equals("image/png")) {
-                result.put("status", "Invalid");
-                return result;
-            }
-            if (profileImg.getSize() > 10000000) {
-                result.put("status", "Invalid");
-                return result;
-            }
-        }
-        if (name == null || name.trim().length() == 0) {
-            result.put("status", "Invalid");
-            return result;
-        }
-        Pattern pattern = Pattern.compile("^[A-Za-z0-9]+ ([0-9]+ ?){1,}$");
-        if (paymentInformation == null || !pattern.matcher(paymentInformation).matches()) {
-            result.put("status", "Invalid");
-            return result;
-        }
-        pattern = Pattern.compile("^(?:[0-9] ?){7,11}$");
-        if (phone == null || !pattern.matcher(phone).matches()) {
-            result.put("status", "Invalid");
-            return result;
-        }
-        if (gender != 0 && gender != 1) {
-            result.put("status", "Invalid");
-            return result;
-        }
-        if (address == null || address.trim().length() == 0) {
-            result.put("status", "Invalid");
-            return result;
-        }
-        District district = districtService.findById(districtID);
-        if (district == null) {
-            result.put("status", "Invalid");
-            return result;
-        }
-        Date parsedBirthday = new Date();
-        if (birthday != null && birthday.trim().length() > 0) {
-            String[] arr = birthday.split("-");
-            try {
-                int year = Integer.parseInt(arr[0]);
-                int month = Integer.parseInt(arr[1]);
-                int day = Integer.parseInt(arr[2]);
-                parsedBirthday = new Date(year - 1900, month - 1, day);
-            } catch (Exception e) {
-                e.printStackTrace();
-                result.put("status", "Invalid");
-                return result;
-            }
-        } 
-        else {
-            parsedBirthday = null;
-        }
-        if (!profileImg.isEmpty()) {
-            if (caterer.getProfileImage() != null) {
-                if (!cloudStorageService.deleteFile("caterer/" + caterer.getProfileImage())) {
-                    result.put("status", "Fail");
-                    return result;
-                }
-            }
-            String fileName = cloudStorageService.generateFileName(profileImg);
-            if (cloudStorageService.uploadFile("caterer/" + fileName, profileImg)) {
-                caterer.setProfileImage(fileName);
-            } 
-            else {
-                result.put("status", "Fail");
-                return result;
-            }
-        }
-        caterer.setFullName(name);
-        caterer.setPaymentInformation(paymentInformation);
-        caterer.setDescription(description);
-        caterer.setPhone(phone);
-        caterer.setGender(gender);
-        caterer.setAddress(address);
-        caterer.setDistrictID(district);
-        if (parsedBirthday != null) {
-            caterer.setBirthday(parsedBirthday);
-        }
-        catererService.save(caterer);
-        result.put("status", "OK");
-        result.put("target", "/auth/toProfile");
-        return result;
-    }
->>>>>>> bac2ad2e3d342cc787d787dc64e02818598ede13
 }
